ff228aadd236df75bf8f2e18c6d17128f7bd5130
Joshua <jgodi96@gmail.com>
2020-11-17 19:13:47 -0800

    Review

--- app.c
+++ app.c
@@ -4,36 +4,11 @@
  *       Filename:  app.c
  *
  *    Description:  This file represents the application demonstrating the use of MLD library
- *
- *        Version:  1.0
- *        Created:  Thursday 28 February 2019 06:41:32  IST
- *       Revision:  1.0
- *       Compiler:  gcc
- *
- *         Author:  Er. Abhishek Sagar, Networking Developer (AS), sachinites@gmail.com
- *        Company:  Brocade Communications(Jul 2012- Mar 2016), Current : Juniper Networks(Apr 2017 - Present)
- *        
- *        This file is part of the MLD distribution (https://github.com/sachinites).
- *        Copyright (c) 2017 Abhishek Sagar.
- *        This program is free software: you can redistribute it and/or modify
- *        it under the terms of the GNU General Public License as published by  
- *        the Free Software Foundation, version 3.
- *
- *        This program is distributed in the hope that it will be useful, but 
- *        WITHOUT ANY WARRANTY; without even the implied warranty of 
- *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
- *        General Public License for more details.
- *
- *        You should have received a copy of the GNU General Public License 
- *        along with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * =====================================================================================
- */
+*/
 
 #include "mld.h"
 #include <memory.h>
 #include <stdlib.h>
-#include <stdio.h>
 
 /*Application Structures*/
 
@@ -61,7 +36,6 @@ main(int argc, char **argv){
 
     /*Step 1 : Initialize a new structure database */
     struct_db_t *struct_db = calloc(1, sizeof(struct_db_t));
-    mld_init_primitive_data_types_support(struct_db);
         
     /*Step 2 : Create structure record for structure emp_t*/
     static field_info_t emp_fields[] = {
@@ -86,32 +60,5 @@ main(int argc, char **argv){
 
     /*Step 4 : Verify the correctness of structure database*/
     print_structure_db(struct_db);
-
-
-
-    /*Working with object database*/
-    /*Step 1 : Initialize a new Object database */
-    object_db_t *object_db = calloc(1, sizeof(object_db_t));
-    object_db->struct_db = struct_db;
-    
-    /*Step 2 : Create some sample objects, equivalent to standard 
-     * calloc(1, sizeof(student_t))*/
-    student_t *abhishek = xcalloc(object_db, "student_t", 1);
-    mld_set_dynamic_object_as_root(object_db, abhishek);
-
-    student_t *shivani = xcalloc(object_db, "student_t", 1);
-    strncpy(shivani->stud_name, "shivani", strlen("shivani"));
-    //abhishek->best_colleage = shivani;
-
-    emp_t *joseph = xcalloc(object_db, "emp_t", 2);
-    mld_set_dynamic_object_as_root(object_db, joseph);
-    joseph->p = xcalloc(object_db, "int", 1);
-
-    print_object_db(object_db);
-
-    run_mld_algorithm(object_db);
-    printf("Leaked Objects : \n");
-    report_leaked_objects(object_db);
-
     return 0;
 }
